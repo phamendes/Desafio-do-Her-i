54682975fa26496e31df092dcb493294fc019a73
Phablo <miranda.phablo@live.com>
2024-10-16 14:33:34 -0300

    finalizado

--- DesafioHeroi.js
+++ DesafioHeroi.js
@@ -1,23 +1,35 @@
-class hero{
-    constructor(nome, idade, tipo){
+class Heroi {
+    constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
-    atacar(){
+
+    atacar() {
         let ataque;
-        switch(this.tipo){
-            case 'Mago':
-                ataque = "usou Magia";
+        switch (this.tipo.toLowerCase()) {
+            case 'mago':
+                ataque = 'usou magia';
                 break;
-            case 'Guerreiro':
-                ataque = "usou Espada";
-            case 'Monge':
-                ataque = "usou Artes Marciais";
-            case "Ninja":
-                ataque = "usou Shuriken";
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'fez um ataque desconhecido';
         }
-        console.log(`O ${this.tipo} atacou usando ${this.ataque}`);
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
     }
 }
 
+// Exemplo de uso:
+const heroi1 = new Heroi('Arthur', 17, 'guerreiro');
+heroi1.atacar();
+
+const heroi2 = new Heroi('Phablo', 32, 'mago');
+heroi2.atacar();
