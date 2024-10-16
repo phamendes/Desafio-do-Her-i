fa368da4d0d4a3122846cea8187ac7c79ed8d4d0
Phablo <miranda.phablo@live.com>
2024-10-16 14:16:53 -0300

    feito o ataque

--- DesafioHeroi.js
+++ DesafioHeroi.js
@@ -4,4 +4,20 @@ class hero{
         this.idade = idade;
         this.tipo = tipo;
     }
+    atacar(){
+        let ataque;
+        switch(this.tipo){
+            case 'Mago':
+                ataque = "usou Magia";
+                break;
+            case 'Guerreiro':
+                ataque = "usou Espada";
+            case 'Monge':
+                ataque = "usou Artes Marciais";
+            case "Ninja":
+                ataque = "usou Shuriken";
+        }
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`);
+    }
 }
+
